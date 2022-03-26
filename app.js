3307ca93aa0603865c2ba6c321a3a9c7f11ffd8b
Катерина <Me@example.com>
2022-03-26 20:09:35 +0200

    Revert "Убрала надпись"
    This reverts commit 2f54a256f6577c03d730fd0d4fa15d7cdf7e34af.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.get('/data/2.5/weather', get_weather)
 
 function get_weather(request, response)
 {
+    console.log("Хтось спитав погоду")
     console.log(request.query.lat)
     response.json({"coord":{"lon":7.2094,"lat":50.082},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations","main":{"temp":18.34,"feels_like":17.14,"temp_min":17.51,"temp_max":20.08,"pressure":1029,"humidity":35},"visibility":10000,"wind":{"speed":6.17,"deg":60},"clouds":{"all":0},"dt":1648311221,"sys":{"type":2,"id":2020334,"country":"DE","sunrise":1648272071,"sunset":1648317144},"timezone":3600,"id":6553725,"name":"Senheim","cod":200})
 }
